0ee340e7f1ac50ca5fdb4a80665883f38b11092b
zykhoo <46671677+zykhoo@users.noreply.github.com>
2022-03-18 22:23:36 +0800

    Update groundtruth_2dim.py

--- synthetic_systems/groundtruth_2dim.py
+++ synthetic_systems/groundtruth_2dim.py
@@ -56,7 +56,7 @@ def CreateTrainingDataTrajClassicInt(traj_len,ini_con,spacedim,h,f1,f2,n_h = 800
   return start,final
 
 def CreateTrainingDataTrajClassicIntRandom(traj_len,ini_con,spacedim,h,f1,f2,seed,n_h = 800):
-  np.random.seed(seed = ini_con)
+  np.random.seed(seed = seed)
   startcon = np.random.uniform(spacedim[0][0], spacedim[0][1], size = ini_con)
   for i in range(len(spacedim)-1):
     startcon = np.vstack((startcon, np.random.uniform(spacedim[i+1][0], spacedim[i+1][1], size = ini_con)))
