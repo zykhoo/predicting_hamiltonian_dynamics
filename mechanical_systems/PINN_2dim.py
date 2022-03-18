7eecb37232a601e72a2a344e5190fc0fa0aa0a9d
zykhoo <46671677+zykhoo@users.noreply.github.com>
2022-03-18 16:55:24 +0800

    Update PINN_2dim.py

--- mechanical_systems/PINN_2dim.py
+++ mechanical_systems/PINN_2dim.py
@@ -293,3 +293,28 @@ def gen_one_trajNNH_autograd(traj_len,start,h,model,device,n_h = 800):
       x=LeapfrogNNH_autograd(x,h_gen,model,device)
     final=np.hstack((final,x))
   return start[:,1:],final[:,1:]
+
+def get_grad(model, z,device):
+    inputs=Variable(torch.tensor([z[0][0],z[1][0]]), requires_grad = True).to(device)
+    out=model(inputs.float())
+    dH=torch.autograd.grad(out, inputs, grad_outputs=out.data.new(out.shape).fill_(1),create_graph=True)[0]
+    return -dH.detach().cpu().numpy()[0] # negative dH/dq is dp/dt
+
+def naiveIntNNH_autograd(z,h,model,device):
+## classical Leapfrog scheme for force field f
+# can compute multiple initial values simultanously, z[k]=list of k-component of all initial values
+	dim = int(len(z)/2)
+	z[dim:] = z[dim:]+h*get_grad(model, z,device)
+	z[:dim] = z[:dim]+h*z[dim:]
+	return z
+  
+def gen_one_trajNNH_naive_autograd(traj_len,start,h,model,device,n_h = 800):
+  h_gen = h/n_h
+  x, final = start.copy(), start.copy()
+  for i in range(traj_len):
+    start=np.hstack((start,x))
+    for j in range(0,int(n_h+1)):
+      x=naiveIntNNH_autograd(x,h_gen,model,device)
+    final=np.hstack((final,x))
+  return start[:,1:],final[:,1:]
+
