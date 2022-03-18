133b7d626633880c94ca88b662c7132192da29c9
zykhoo <46671677+zykhoo@users.noreply.github.com>
2022-03-18 16:03:07 +0800

    Update GP_2dim.py

--- mechanical_systems/GP_2dim.py
+++ mechanical_systems/GP_2dim.py
@@ -20,3 +20,21 @@ def gen_one_trajGP(traj_len,start,h,gaussian_process,scaler,n_h = 800):
       x=LeapfrogGP(x,h_gen,gaussian_process,scaler)
     final=np.hstack((final,x))
   return start[:,1:],final[:,1:]
+
+def naiveIntGP(z,h,gaussian_process,scaler):
+## classical Leapfrog scheme for force field f
+# can compute multiple initial values simultanously, z[k]=list of k-component of all initial values
+	dim = int(len(z)/2)
+	z[dim:] = z[dim:]+h*gaussian_process.predict(scaler.transform(z.transpose()))[0][1]
+	z[:dim] = z[:dim]+h*z[dim:]
+	return z
+  
+def gen_one_traj_naiveGP(traj_len,start,h,gaussian_process,scaler,n_h = 800):
+  h_gen = h/n_h
+  x, final = start.copy(), start.copy()
+  for i in range(traj_len):
+    start=np.hstack((start,x))
+    for j in range(0,int(n_h+1)):
+      x=naiveIntGP(x,h_gen,gaussian_process,scaler)
+    final=np.hstack((final,x))
+  return start[:,1:],final[:,1:]
