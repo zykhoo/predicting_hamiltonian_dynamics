8d4d852a442ac4e5d82ece056c58ad242fcba112
zykhoo <46671677+zykhoo@users.noreply.github.com>
2022-03-18 19:40:38 +0800

    Update GP_2dim.py

--- synthetic_systems/GP_2dim.py
+++ synthetic_systems/GP_2dim.py
@@ -2,6 +2,27 @@
 
 import numpy as np 
 
+def naiveIntGP(z,h,gaussian_process,scaler):
+		dim = int(len(z)/2)
+		q=z[:dim]
+		p=z[dim:]		
+		dH = gaussian_process.predict(scaler.transform(z.transpose()))[0]
+		q = q+h*dH[0]
+		p = p + h*dH[1]
+		return np.block([q,p])
+
+def naiveTrajectoryGP(z,h,gaussian_process,scaler,N=1):
+	## trajectory computed with classicInt
+  z = z.reshape(1,-1)[0]
+  trj = np.zeros((len(z),N+2))
+  trj[:,0] = z.copy()
+  if N == 1:
+    return z.reshape(-1,1), naiveInt(trj[:,0].reshape(-1,1),h,gaussian_process,scaler).reshape(-1,1)
+  else:
+    for j in range(0,N+1):
+      trj[:,j+1] = naiveInt(trj[:,j].reshape(-1,1).copy(),h,gaussian_process,scaler)
+  return trj[:, :-1], trj[:, 1:]
+
 def classicIntGP(z,h,gaussian_process,scaler):
 	## classical symplectic Euler scheme
 		dim = int(len(z)/2)
