80ff9e48a7d3a47a3df20429e4bd174ab4ccf24a
zykhoo <46671677+zykhoo@users.noreply.github.com>
2022-03-27 00:42:40 +0800

    Update GP_2dim.py

--- synthetic_systems/GP_2dim.py
+++ synthetic_systems/GP_2dim.py
@@ -38,7 +38,6 @@ def classicIntGP(z,h,gaussian_process,scaler):
 			stageold = stage+0.
 			stage = fstage(stage)+0.
 			Iter = Iter+1
-		print("Iterations", Iter)
 		q = q+stage
 		p = p + h*gaussian_process.predict(scaler.transform(np.concatenate([q,p]).transpose()))[0][1]
 		return np.block([q,p])
